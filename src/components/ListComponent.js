4fcc086c95c0ae22bb26d5c20d3f5dfe1b774876
Ananias <ananias.nobrega@gmail.com>
2020-07-06 16:23:43 -0300

    implements ListComponent

---
+++ src/components/ListComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export default function ListComponent(){
+    return (
+        <div>
+            <h2>
+                Minha Lista
+            </h2>
+            <ul>
+                <li>Item 01</li>
+                <li>Item 02</li>
+                <li>Item 03</li>
+            </ul>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ListComponent from './components/ListComponent';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ListComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
