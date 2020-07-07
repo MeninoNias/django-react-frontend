bf9de23ec59bc50f5aeb8599a0892a8c684e3a70
Ananias <ananias.nobrega@gmail.com>
2020-07-06 23:13:13 -0300

    implements init API

--- src/components/UserComponent.js
+++ src/components/UserComponent.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ListComponent from './ListComponent';
 
-export default function UserComponent() {
+export default class UserComponent extends React.Component {
 
-    return (
-        <div>
-            <ListComponent listname= { 'Lista Teste 01' }/>
-            <ListComponent listname= { 'Lista Teste 02' }/>
-        </div>
-    )
+    state = {
+        lists: null, loading: true
+    }
+
+    async componentDidMount(){
+        var url = 'http://127.0.0.1:8000/list/';
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data)
+        this.state({lists:data, loading:false})    
+    }
+
+    render(){
+        return (
+            <div>
+                <ListComponent listname= { 'Lista Teste 01' }/>
+                <ListComponent listname= { 'Lista Teste 02' }/>
+            </div>
+        )
+    } 
     
 }
